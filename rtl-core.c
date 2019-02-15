4796af97fa386cea6b472bd05ca72a5f80db490b
HyunJun Jeong <rollrat@inha.edu>
2019-02-15 18:20:45 +0900

    Add files via upload

--- rtl-core.c
+++ rtl-core.c
@@ -20,7 +20,7 @@
 
 typedef uint32_t major_type;
 
-static int run_fault_injection = 1;
+static int run_fault_injection = 0;
 // 미리 테이블을 만듦
 static int make_pre_table = 0;
 // 대상 레지스터 수
@@ -36,14 +36,10 @@ static int check_index[MAX_INDEX];
 static int check_count_require[MAX_INDEX];
 static int check_count[MAX_INDEX];
 
-//static int profile_count = 0;
-//static int profile_index[MAX_INDEX * 4];
-//static int profile_value[MAX_INDEX * 4];
-
 void fault_inject_init(uint32_t _count_of_index)
 {
 #if PRINT_MESSAGE
-  printf("fault-injector rtl-core init!\n");
+  printf("[Fault Injector] RTL-Core Init!\n");
 #endif
 
   srand(time(NULL));
@@ -66,9 +62,28 @@ void fault_inject_init(uint32_t _count_of_index)
       check_index[chk] = 1;
       drawing_lots = 0;
     }
-  } else {
+  }
+  else {
     drawing_lots = 1;
   }
+}
+
+void fault_inject_trace(
+  uint32_t f_index,
+  uint32_t index,
+  uint32_t reg_num,
+  uint32_t dependency,
+  uint32_t size,
+  char *value,
+  char *opcode)
+{
+  printf("[Fault Injector] f_index=%02d, index=%02d, reg_num=%02d, dependency=%d, opcode=%7s, size=%02d, value=", f_index, index, reg_num, dependency, opcode, size);
+
+  int sz = size / 8;
+  for (int i = 0; i < sz; i++)
+    printf("%02X", value[sz - i - 1]);
+
+  printf("\n");
 }
 
 static int determine(double rate)
@@ -76,21 +91,10 @@ static int determine(double rate)
   return (double)rand() / RAND_MAX >= rate;
 }
 
-void fault_inject_profile(
-  major_type f_index,
-  major_type index, 
-  char *value,
-  char *name) {
-  /*if (profile_count == MAX_INDEX * 4)
-    return;
-  profile_index[profile_count] = index;
-  profile_value[profile_count] = value;
-  profile_count++;*/
-}
-
 major_type fault_inject_determine(
   major_type f_index,
   major_type index,
+  major_type reg_num,
   major_type dependency)
 {
   if (run_fault_injection == 0)
@@ -131,8 +135,11 @@ void fault_inject_set1(
 void fault_inject(
   uint32_t f_index,
   uint32_t index,
+  uint32_t reg_num,
+  uint32_t dependency,
   uint32_t size,
-  char *value) {
+  char *value,
+  char *opcode) {
   switch (injection_type) {
   case 0: fault_inject_flipbit(value); break;
   case 1: fault_inject_set0(value);    break;
@@ -143,7 +150,7 @@ void fault_inject(
 void fault_inject_finish()
 {
 #if PRINT_MESSAGE
-  printf("fault-injector rtl-core finish!\n");
+  printf("[Fault Injector] RTL-Core Finish!\n");
 #endif
 
   //
